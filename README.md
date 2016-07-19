71ddc75cd6afcded023ee43dbaab88b9de5e5186
karupanerura <karupa@cpan.org>
2016-07-19 19:58:16 +0900

    initialize commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Plack::Middleware::Auth::OpenID - It's new $module
+
+# SYNOPSIS
+
+    use Plack::Middleware::Auth::OpenID;
+
+# DESCRIPTION
+
+Plack::Middleware::Auth::OpenID is ...
+
+# LICENSE
+
+Copyright (C) karupanerura.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+karupanerura <karupa@cpan.org>
---
+++ lib/Plack/Middleware/Auth/OpenID.pm
@@ -0,0 +1,39 @@
+package Plack::Middleware::Auth::OpenID;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Plack::Middleware::Auth::OpenID - It's new $module
+
+=head1 SYNOPSIS
+
+    use Plack::Middleware::Auth::OpenID;
+
+=head1 DESCRIPTION
+
+Plack::Middleware::Auth::OpenID is ...
+
+=head1 LICENSE
+
+Copyright (C) karupanerura.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+karupanerura E<lt>karupa@cpan.orgE<gt>
+
+=cut
+
